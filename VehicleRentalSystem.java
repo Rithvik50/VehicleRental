ab4563d1db507a1460991a15c717e074794545ed
ROHAN S PES2UG22CS456 <rohanshivu2003@gmail.com>
2025-03-12 12:23:22 +0530

    Updated size

--- VehicleRentalSystem.java
+++ VehicleRentalSystem.java
@@ -6,7 +6,7 @@ public class VehicleRentalSystem {
         Color background = new Color(8, 124, 168);
 		JFrame w = new JFrame("Vehicle Rental System");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		w.setBounds(0, 0, (int)screenSize.getWidth(), (int)screenSize.getHeight() - 100);
+		w.setBounds(0, 0, (int)screenSize.getWidth(), (int)screenSize.getHeight());
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		w.getContentPane().setBackground(background);
 		w.setLayout(new FlowLayout(FlowLayout.CENTER));

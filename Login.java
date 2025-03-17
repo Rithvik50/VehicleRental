371d60b53e36e9a2de02e2c0cdaf7cb5012e81fd
Rithvik50 <rmuthyalapati@gmail.com>
2025-03-17 20:53:25 +0530

    Added test button to the login page and register page

--- Login.java
+++ Login.java
@@ -43,19 +43,32 @@ public class Login extends MouseAdapter {
                     }
                 }
             } else if (pages == LOGIN_PAGES.LOGIN_PAGE) {
-
+                if (mX >= 600 && mX <= 800 && mY >= 300 && mY <= 350) {
+                    pages = LOGIN_PAGES.MENU_PAGE;
+    
+                    if (source instanceof Window) {
+                        Window window = (Window) source;
+                        window.repaint();
+                    }
+                }
             } else if (pages == LOGIN_PAGES.REGISTER_PAGE) {
-
+                if (mX >= 600 && mX <= 800 && mY >= 300 && mY <= 350) {
+                    pages = LOGIN_PAGES.MENU_PAGE;
+    
+                    if (source instanceof Window) {
+                        Window window = (Window) source;
+                        window.repaint();
+                    }
+                }
             }
         }
     }
 
     public void render(Graphics g) {
+        FontMetrics fm = g.getFontMetrics();
         if (pages == LOGIN_PAGES.MENU_PAGE) {
             g.fillRect(600, 300, 200, 50);
             g.fillRect(600, 400, 200, 50);
-
-            FontMetrics fm = g.getFontMetrics();
 
             int loginTextWidth = fm.stringWidth("Login");
             int loginX = 600 + (200 - loginTextWidth) / 2;
@@ -68,8 +81,23 @@ public class Login extends MouseAdapter {
             int registerY = 400 + (50 + fm.getAscent()) / 2;
             g.drawString("Register", registerX, registerY);
         } else if (pages == LOGIN_PAGES.LOGIN_PAGE) {
-            g.drawString("On Login Page", 500, 100);
+            g.fillRect(600, 300, 200, 50);
+
+            int loginTextWidth = fm.stringWidth("Back");
+            int loginX = 600 + (200 - loginTextWidth) / 2;
+            int loginY = 300 + (50 + fm.getAscent()) / 2;
+
+            g.setColor(Color.BLACK);
+            g.drawString("Back", loginX, loginY);
         } else if (pages == LOGIN_PAGES.REGISTER_PAGE) {
+            g.fillRect(600, 300, 200, 50);
+
+            int loginTextWidth = fm.stringWidth("Back");
+            int loginX = 600 + (200 - loginTextWidth) / 2;
+            int loginY = 300 + (50 + fm.getAscent()) / 2;
+
+            g.setColor(Color.BLACK);
+            g.drawString("Back", loginX, loginY);
             g.drawString("On Rental Page", 500, 100);
         } 
     }

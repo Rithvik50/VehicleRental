bddd948a335d3314bbf78cb53f8cd8ccda38a8ca
Rithvik50 <rmuthyalapati@gmail.com>
2025-04-21 09:25:13 +0530

    commit

--- src/Rental.java
+++ src/Rental.java
@@ -1,78 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Rental extends MouseAdapter {
-    private List<User> users;
-    private List<Vehicle> availableVehicles;
     private Window window;
 
     public Rental(Window window) {
-        users = new ArrayList<>();
-        availableVehicles = new ArrayList<>();
         this.window = window;
-    }
-
-    public void addUser(User user) {
-        users.add(user);
-    }
-
-    public void addVehicle(Vehicle vehicle) {
-        availableVehicles.add(vehicle);
-    }
-
-    public void rentVehicle(String userId, String regnNumber) {
-        User user = findUser(userId);
-        Vehicle vehicle = findVehicle(regnNumber);
-
-        if (user != null && vehicle != null) {
-            user.rentVehicle(vehicle);
-            availableVehicles.remove(vehicle);
-            System.out.println("Vehicle with registration number " + regnNumber + 
-                               " rented successfully to ");
-        } else {
-            System.out.println("Rental failed. User or vehicle not found.");
-        }
-    }
-
-    private User findUser(String userId) {
-        for (User user : users) {
-            if (user.getUserId().equals(userId)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    private Vehicle findVehicle(String regnNumber) {
-        for (Vehicle vehicle : availableVehicles) {
-            if (vehicle.getRegnNumber().equals(regnNumber)) {
-                return vehicle;
-            }
-        }
-        return null;
-    }
-
-    public void displayUserRentals(String userId) {
-        User user = findUser(userId);
-        if (user != null) {
-            user.displayRentedVehicles();
-        } else {
-            System.out.println("User not found.");
-        }
-    }
-
-    public void displayAvailableVehicles() {
-        System.out.println("Available Vehicles:");
-        if (availableVehicles.isEmpty()) {
-            System.out.println("No vehicles available for rent.");
-        } else {
-            for (Vehicle vehicle : availableVehicles) {
-                vehicle.displayDetails();
-            }
-        }
     }
 
     @Override
